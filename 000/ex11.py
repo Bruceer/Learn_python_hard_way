f997a8ae448cad46747ca4976a1f45744d34b79c
Bruceer <tsezz@qq.com>
2019-04-05 19:39:08 +0800

    add ex8~15

---
+++ 000/ex11.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+age = input(" How old are your? " )
+heigh = input("How tall are your? ")
+weight = input("How much do your weight? ")
+
+print("So, you are %r old, %r tall and %r heavy." %(age, heigh, weight))
---
+++ 000/ex9.py
@@ -0,0 +1,12 @@
+
+# Here's some new strange stuff, remember type it exactly.
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = " \nJan \nFeb \nMar \nApr \nMay \nJun \nJul \nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# three double-quote is importance
+print("""There's something going on here,
+With the threee double-quotes.
+We'll be able to type as much as we like.""")
